938e7dec26b400bb6c4cb6b4618c272af168c718
leventbk <leventbkarakus@hotmail.com>
2020-05-12 09:20:26 -0400

    app update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
 
-class App extends React.Component {
-  render (){
+class App extends Component {
+  render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar/><BusinessList/>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
 }
 
-export default  App;
+export default App;
