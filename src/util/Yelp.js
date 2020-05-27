237fc2a9bd04e245f6a35768aafcfafc385dc6fe
levent karakuş <leventbkarakus@hotmail.com>
2020-05-27 18:24:09 -0400

    working on Yelp API_2

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,8 +2,7 @@ const apiKey = 'enter api key';
 
 const yelp = {
     search(term, location, sortBy) {
+        //bypass CORS restriction via cors-anywhere
         return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
     }
 };
-
-
