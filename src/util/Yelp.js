1d87e07010356164d84b52dccd93e45b8176fb68
levent karakuş <leventbkarakus@hotmail.com>
2020-05-29 21:08:59 -0400

    working on yelp api

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,6 +3,13 @@ const apiKey = 'enter api key';
 const yelp = {
     search(term, location, sortBy) {
         //bypass CORS restriction via cors-anywhere
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+            {headers: {Authorization: `Bearer ${apiKey}`}}
+        ).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(){}
+        })
     }
 };
